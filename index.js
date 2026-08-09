ed10348f36110c3f42575a14e23620fee16a9576
Pulkit2007 <bansalpulkit2007@outlook.com>
2026-08-09 19:46:27 +0530

    Making a counter

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 
-const PI = 3.14;
-let radius;
-let circum
+const countlabel = document.getElementById("countlabel");
+const decreasebtn = document.getElementById("decreasebtn");
+const resetbtn = document.getElementById("resetbtn");
+const increasebtn = document.getElementById("increasebtn");
 
-document.getElementById('mysubmit').onclick = function(){
-    radius = document.getElementById('mytext').value ;
-    radius = Number(radius)
-    circum = 2 * PI * radius
-    document.getElementById("myh3").textContent = circum
-    
+let count = 0;
 
+increasebtn.onclick=function(){
+    count+=1;
+    countlabel.textContent = count;
 }
+resetbtn.onclick=function(){
+    count=0 ;
+    countlabel.textContent = count;
+}
+decreasebtn.onclick=function(){
+    count-=1;
+    countlabel.textContent = count;
+}
+
