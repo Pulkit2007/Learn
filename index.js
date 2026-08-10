baf5e7d8c233d5df255f3dbb3f8d21869e6b11e0
Pulkit2007 <bansalpulkit2007@outlook.com>
2026-08-10 21:20:22 +0530

    adding tutorials

--- index.js
+++ index.js
@@ -1,29 +1,17 @@
 
-const mytext = document.getElementById("mytext");
-const mybutton = document.getElementById("mybutton");
-const myoutput = document.getElementById("myoutput");
-let age;
+// this file is only for testing purposes it may be empty in github
+// all the actual code files can be found in tutorials section
 
+let username = "pulkit bansal";
 
-mybutton.onclick = function(){
+console.log(username.repeat(3))
+//repeat the username 3 times
 
-    age = mytext.value;
-    age = Number(age);
+console.log(username.startsWith("p"))
+//output : True
 
+console.log(username.includes(" "))
+//output : True
 
-    if(age >=100){
-    myoutput.textContent = "You are too Old to Enter this site"
-}
-    else if(age >= 18){
-    myoutput.textContent = "You are enough to Enter this site"
-}
-    else if(age < 0 ){
-    myoutput.textContent = "You are just born to Enter this site"
-}
-    else{
-    myoutput.textContent = "You must be Old to Enter this site"
-}
-}
-
-
-
+console.log(username.replaceAll(" ", ""))
+//output : pulkitbansal
---
+++ tutorials/switch.js
@@ -0,0 +1,25 @@
+let day =1;
+
+switch(day){
+    case 1:
+        console.log(`Its Monday`)
+        break;
+    case 2:
+        console.log(`Its Tuesday`)
+        break;
+    case 3:
+        console.log(`Its Wednesday`)
+        break;
+    case 4:
+        console.log(`Its Thursday`)
+        break;
+    case 5:
+        console.log(`Its Akhi na tuh putt jatt da! ohhhh  ohh`)
+        break;
+    case 6:
+        console.log(`Its Saturday`)
+        break;
+    case 7:
+        console.log(`Its Sunday`)
+        break;       
+}
